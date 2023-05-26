0cf992dfffe676e321965461817286df99344ee0
anura <anurag.console@gmail.com>
2023-05-27 04:28:11 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,46 @@
+var typed = new Typed (".multi",{
+    strings: ["Frontend Developer" , "UI/UX Designer" , "Backend Developer " ,
+     "Android Developer" ] ,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+}) 
+
+let docTitle = document.title;
+window.addEventListener("blur", () => {document.title = "Come BAck Later";});
+
+window.addEventListener("focus", () => { document.title = docTitle; });
+
+
+
+// scroll section logic
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll( 'header nav a');
+
+window.onscroll = () => {
+sections. forEach(sec => {
+let top = window.scrollY;
+let offset = sec.offsetTop - 150;
+let height = sec.offsetHeight;
+let id = sec.getAttribute('id');
+
+if(top >= offset && top < offset + height) {
+navLinks.forEach(links => {
+links.classList.remove('active');
+document .querySelector( 'header nav a[href*=' + id + "]").classList.add( 'active');
+
+});
+
+};
+});
+};
+
+/*==============================Sticky Nav Bar==================================*/
+
+let header = document.querySelector('header ');
+
+header.classList.toggle('sticky' , window.scrollY > 100);
+
